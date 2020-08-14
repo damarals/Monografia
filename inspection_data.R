e101f0291d4c935bd049cf64040dbfe7cf92b72a
Daniel Amaral <silva.daniel86@Gmail.com>
2020-08-14 19:38:37 -0300

    dados inserido em /data e código de leitura dos dados criado

--- inspection_data.R
+++ inspection_data.R
@@ -8,13 +8,31 @@
 ############################################################################
 
 
-###  This .R archive makes a load of the two files in ./files and preprocess   
-###  the hive and apiary for get the data final of inspections                 
+###  This .R archive makes a load of the two files in ./files 
+###  and preprocess the hive and apiary for get the data final 
+###  of inspections.                
 
 
 ##---------------------------------------------------------------
 ##                          Libraries                          --
 ##---------------------------------------------------------------
+
+library(tidyverse) # Data Manipulation
+library(readODS)   # Read .ods Files
+
+
+##---------------------------------------------------------------
+##                        Load Datasets                        --
+##---------------------------------------------------------------
+
+###  The inspection files are obtained through 2 spreadsheets in 
+###  .ods format, one containing the inspections of all apiaries 
+###  in the years 2016 and 2017 and the other containing the 
+###  inspections in the year 2018.
+
+inspec1617 <- read_ods('data/inspection/2016-2017_Inspection_Table.ods', 
+                      skip = 1, col_names = T)
+inspec18 <- read_ods('DADOS_INSPECAO/2018_Inspection_Table.ods')
 
 ##----------------------------------------------------------------
 
