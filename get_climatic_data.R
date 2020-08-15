4b859e81b95800652ec47d70195f736771e67d59
Daniel Amaral <silva.daniel86@Gmail.com>
2020-08-15 14:24:47 -0300

    primeira versão da função de dados climáticos criada e arquivo de chave criado externamente

--- get_climatic_data.R
+++ get_climatic_data.R
@@ -18,4 +18,30 @@
 
 library(tidyverse) # Data Manipulation
 library(lubridate) # Date Manipulation
+library(httr)      # Work with URLs and HTTP
+library(rjson)     # JSON Manipulation
 
+
+##---------------------------------------------------------------
+##                        Core Function                        --
+##---------------------------------------------------------------
+
+getClimaticData <- function(date, lat, lng) {
+  
+  ApiBaseURL = 'http://api.worldweatheronline.com/premium/v1/past-weather.ashx?'
+  source('./api_key.R')
+  
+  query <- URLencode(paste(ApiBaseURL, 
+                     paste0('q=', lat, ',', lng), 
+                     paste0('date=', date),
+                     paste0('key=', ApiKey),
+                     'format=json&includelocation=yes&tp=1', sep = '&'))
+
+  getData <- GET(url = query)
+  
+  return(getData)
+}
+
+
+############################################################################
+############################################################################
