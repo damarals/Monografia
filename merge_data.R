9b67174e0a03a0ba02b75e71589c6e97bf767eb9
Daniel Amaral <silva.daniel86@Gmail.com>
2020-08-16 00:51:29 -0300

    arquivo para o merge dos datasets criado e dados lidos

---
+++ merge_data.R
@@ -0,0 +1,35 @@
+
+###########################################################################
+###########################################################################
+###                                                                     ###
+###             MERGE DATA: INSPECTIONS + CLIMATE + SENSORS             ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
+###  This .R file reads the 2 data sets in ./data and merges. 
+###  First, the climatic data are obtained through the inspection 
+###  data and later aggregated. After that, an inner join is 
+###  performed between the results data from the first stage and 
+###  the sensor data.
+
+
+##---------------------------------------------------------------
+##                          Libraries                          --
+##---------------------------------------------------------------
+
+library(tidyverse) # Data Manipulation
+library(lubridate) # Date Manipulation
+
+
+##---------------------------------------------------------------
+##                        Load data sets                       --
+##---------------------------------------------------------------
+
+###  The data files are obtained through 2 spreadsheets in the
+###  format .csv, containing inspections of all apiaries in the 
+###  years 2016, 2017 and 2018 and the sensor data summarized 
+###  by day.
+
+inspections <- read_csv('./data/inspections.csv')
+sensors <- read_csv('./data/sensors.csv')
